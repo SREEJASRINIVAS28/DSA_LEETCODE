f1f0d84ea0d4af060c5768047826656e56284a9a
SREEJASRINIVAS28 <srijagedela57@gmail.com>
2025-08-10 16:24:28 +0530

    Time: 9 ms (28.16%) | Memory: 17 MB (33.69%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-   void PrintSum(int i,int &n,vector<int> &candidates,vector<int> &res,vector<vector<int>> &ans,int sum,int &target){
-    if(i>=n){
+set<vector<int>> s;
+void PrintSum(vector<int>& candidates,int i,vector<int> &res,vector<vector<int>> &ans,int tar){
+
+    if(i==candidates.size() || tar<0){
         return;
     }
-    if(sum==target){
-        ans.push_back(res);
-        return;
+    if(tar==0){
+        if(s.find(res)==s.end()){
+            ans.push_back(res);
+            s.insert(res);
+            return;
+        }
     }
-
-    if(sum+candidates[i]<=target){
-        res.push_back(candidates[i]);
-        PrintSum(i,n,candidates,res,ans,sum+candidates[i],target);
-         res.pop_back();
-
-    }
-    PrintSum(i+1,n,candidates,res,ans,sum,target);
-   }
+    res.push_back(candidates[i]);
+    PrintSum(candidates,i+1,res,ans,tar-candidates[i]);
+    PrintSum(candidates,i,res,ans,tar-candidates[i]);
+    res.pop_back();
+    PrintSum(candidates,i+1,res,ans,tar);
+}
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-       int n=candidates.size();
-       vector<int> res;
-       vector<vector<int>> ans;
-       PrintSum(0,n,candidates,res,ans,0,target);   
-       return ans;
+        vector<int> res;
+        vector<vector<int>> ans;
+        PrintSum(candidates,0,res,ans,target);
+        return ans;
     }
 };
-
-
