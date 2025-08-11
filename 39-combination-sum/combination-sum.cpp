dddd8de23ed26cfdbec45993520419b9779b761b
SREEJASRINIVAS28 <srijagedela57@gmail.com>
2025-08-11 18:14:33 +0530

    Time: 6 ms (34.12%) | Memory: 14 MB (72.75%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-set<vector<int>> s;
+// set<vector<int>> s;
 void PrintSum(vector<int>& candidates,int i,vector<int> &res,vector<vector<int>> &ans,int tar){
 
+    
+    if(tar==0){
+        // if(s.find(res)==s.end()){
+        //     ans.push_back(res);
+        //     s.insert(res);
+        //     return;
+        // }
+         ans.push_back(res);
+         return;
+    }
     if(i==candidates.size() || tar<0){
         return;
     }
-    if(tar==0){
-        if(s.find(res)==s.end()){
-            ans.push_back(res);
-            s.insert(res);
-            return;
-        }
-    }
     res.push_back(candidates[i]);
-    PrintSum(candidates,i+1,res,ans,tar-candidates[i]);
+    // PrintSum(candidates,i+1,res,ans,tar-candidates[i]);
     PrintSum(candidates,i,res,ans,tar-candidates[i]);
     res.pop_back();
     PrintSum(candidates,i+1,res,ans,tar);
