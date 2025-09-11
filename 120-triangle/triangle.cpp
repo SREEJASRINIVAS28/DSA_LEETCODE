ff1937e5eb9d8ee2fa80124aa8971d442f4694d2
SREEJASRINIVAS28 <srijagedela57@gmail.com>
2025-09-11 18:33:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (82.24%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -9,7 +9,7 @@ public:
 
         }
         for(int i=m-2;i>=0;i--){
-            for(int j=0;j<=i;j++){
+            for(int j=i;j>=0;j--){
                 int down=triangle[i][j]+front[j];
                 int diag=triangle[i][j]+front[j+1];
                 cur[j]=min(down,diag);
