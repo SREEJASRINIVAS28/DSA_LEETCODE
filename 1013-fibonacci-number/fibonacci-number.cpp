588dd00579a5b391cfedc9750141a40f8da3ffc5
SREEJASRINIVAS28 <srijagedela57@gmail.com>
2025-09-13 02:10:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (11.33%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+int f(int n,vector<int> &dp){
+    if(n==0) return 0;
+    if(n==1) return 1;
+    if(dp[n]!=-1) return dp[n];
+    int left=f(n-1,dp);
+    int right=f(n-2,dp); 
+    return dp[n]=left+right;
+    }
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        return f(n,dp);
+        
+    }
+};
